59262712c82497670f2b0c773e1c75f962c5a266
Henrique Souza <johanhenrique2k24@gmail.com>
2024-08-10 02:07:51 -0300

    fix(styles): Responsive

--- src/app/campanhas/[campanhaid]/page.tsx
+++ src/app/campanhas/[campanhaid]/page.tsx
@@ -36,16 +36,14 @@ const PainelControl = () => {
   return (
     <div>
       <CampaignEdit isOpen={isOpen} setIsOpen={handleOpenItem} />
-      <ul className="grid grid-cols-5 space-x-2">
-        {Array.from({ length: 3 }, (_, i) => (
+      <ul className="grid  grid-cols-4 lg:grid-cols-3 h-auto md:grid-cols-1 sm:grid-cols-1 overflow-y-auto space-x-2 md:space-x-0 sm:space-x-0 md:space-y-2 sm:space-y-2">
+        {Array.from({ length: 1 }, (_, i) => (
           <Card key={i} onClick={handleOpenItem} className="py-3">
             <CardHeader className="space-y-4">
               <CardTitle>Configuração 1</CardTitle>
               <CardDescription>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam
-                temporibus repellendus sit et aperiam culpa pariatur! Voluptate
-                adipisci eligendi aperiam animi fuga veritatis quaerat ut nulla.
-                Fugiat qui nisi quasi.
+                temporibus repellendus.
               </CardDescription>
             </CardHeader>
 
@@ -63,9 +61,9 @@ const PainelControl = () => {
 
 export default function CampanhaPage() {
   return (
-    <div className="center-col w-full h-screen font-poppins_light sm:items-start py-5 pl-7 px-4">
+    <div className="center-col w-full h-screen font-poppins_light sm:items-start py-5 pl-7 px-4 overflow-y-auto">
       <PageHead title="Campanha Teste Grapa" subtitle="#123123123123" />
-      <div className="w-full center  mt-4 !justify-start">
+      <div className="w-full center md:center-col md:!items-start md:space-y-2 sm:center-col sm:!items-start sm:space-y-2  mt-4 !justify-start">
         <div className="center !w-auto space-x-3 tags tags-page whitespace-nowrap sm:center-col sm:!space-x-0 sm:!items-start sm:space-y-2">
           <span className="archived">Arquivada</span>
           <span className="social">
@@ -74,10 +72,10 @@ export default function CampanhaPage() {
           </span>
         </div>
         <Separator
-          className="h-[80%] mx-3 w-[1px] bg-gray-700"
+          className="h-[80%] mx-3 md:hidden sm:hidden w-[1px] bg-gray-700"
           orientation="vertical"
         />
-        <div className="center space-x-2 text-gray-800">
+        <div className="center sm:center-col sm:!items-start sm:space-x-0 sm:space-y-2 space-x-2 text-gray-800">
           <div className="center space-x-2">
             <CalendarFoldIcon strokeWidth={1} />
             <span>Inicio: 01/01/2022</span>
@@ -98,13 +96,21 @@ export default function CampanhaPage() {
       </div>
       <Separator className="mt-3" />
       <Tabs defaultValue="painel" className="w-full mt-5 h-full">
-        <TabsList>
-          <TabsTrigger value="painel">Painel de controle</TabsTrigger>
-          <TabsTrigger value="conteudo">Conteudo</TabsTrigger>
-          <TabsTrigger value="recrutamento">Recrutamento</TabsTrigger>
-          <TabsTrigger value="relatorios">Relátorios</TabsTrigger>
+        <TabsList className="sm:center-col h-auto">
+          <TabsTrigger value="painel" className="w-full">
+            Painel de controle
+          </TabsTrigger>
+          <TabsTrigger value="conteudo" className="w-full">
+            Conteudo
+          </TabsTrigger>
+          <TabsTrigger value="recrutamento" className="w-full">
+            Recrutamento
+          </TabsTrigger>
+          <TabsTrigger value="relatorios" className="w-full">
+            Relátorios
+          </TabsTrigger>
         </TabsList>
-        <TabsContent value="painel" className="overflow-y-scroll">
+        <TabsContent value="painel" className="overflow-y-scroll ">
           <PainelControl />
         </TabsContent>
         <TabsContent value="conteudo"></TabsContent>
--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -9,11 +9,11 @@ const PageHead: React.FC<PageHeadProps> = ({ title, subtitle }) => {
   return (
     <div className="w-full font-poppins center !justify-start ">
       <div className="center ">
-        <h1 className="text-[3.4rem] sm:text-[1.9rem] font-[900] leading-[3.4rem]">
+        <h1 className="text-[3.4rem] md:text-[2.5rem] sm:text-[1.9rem] sm:leading-[1.9rem] sm:mt-10 font-[900] leading-[3.4rem]">
           {title}
         </h1>
         {subtitle && (
-          <p className="text-[1.1rem] opacity-80 sm:text-[0.7rem] sm:hidden leading-[1.1rem] ml-2 translate-y-2">
+          <p className="text-[1.1rem] opacity-80 sm:text-[0.7rem] sm:hidden md:hidden leading-[1.1rem] ml-2 translate-y-2">
             / {subtitle}
           </p>
         )}
